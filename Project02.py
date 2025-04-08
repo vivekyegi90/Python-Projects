80100c4de91b1bd00199219f3bee105abad9c669
Vivek Vamsi Yegi <vivekvamsi90@gmail.com>
2025-04-08 17:10:39 +0530

    Add files via upload

---
+++ Project02.py
@@ -0,0 +1,32 @@
+def get_number(number):
+    while True:
+        operand = input("Number " + str(number) + ":" )
+        try:
+            return float(operand)
+        except:
+            print("Invalid number, try again.")
+
+
+operand = get_number(1)
+operand2 = get_number(2)
+sign = input("Sign: ")
+
+
+result = 0
+if sign == "+":
+    result = operand + operand2
+elif sign == "-":
+    result = operand - operand2
+elif sign == "/":
+    if operand2 != 0:
+        result = operand / operand2
+    else:
+         print("not valid")
+elif sign == "*":
+    result = operand * operand2
+else:
+    print("Invalid sign: ")
+    
+print(result)
+
+
